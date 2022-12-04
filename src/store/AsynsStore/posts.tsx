ee9903a779f8e1f8332e0502242759ca0263e78f
fomkinwork <fomkinwork@gmail.com>
2022-12-04 20:15:13 +0300

    new key

--- src/store/AsynsStore/posts.tsx
+++ src/store/AsynsStore/posts.tsx
@@ -1,36 +1,36 @@
-export const getPosts = async (handler: any) => {
+export const getPosts = async (handler?: any) => {
   const response = await fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/', {
       method: "GET",
       headers: {
-          'X-API-KEY': '04a2ea85-90da-42e4-9240-0471c2be5d51',
+          'X-API-KEY': 'abd02b1b-dde9-46ba-8064-05bb7110fda3',
           "Content-Type": "application/json"
       }
   })
       .then(response => response.json())
       .catch(e => console.log(e))
-    handler(response.items)
-    console.log(response.items); 
+    handler(response.items);
+    console.log(response.items)
 }
 
 export const getTrendsPosts = async (handler: any) => {
     const response = await fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/top/', {
         method: "GET",
         headers: {
-            'X-API-KEY': 'de275520-e304-476e-9a0d-a973f6702948',
+            'X-API-KEY': 'abd02b1b-dde9-46ba-8064-05bb7110fda3',
             "Content-Type": "application/json"
         }
     })
         .then(response => response.json())
         .catch(e => console.log(e))
-    handler(response.films)
-    console.log(response.films);
+    handler(response.films);
+    console.log(response.films)
 }
 
 export const getSearchPosts = async (handler:any,keyword:string, pagesCount=1) => {
     const response = await fetch(`https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${keyword}&page=${pagesCount}`, {
         method: "GET",
         headers: {
-            'X-API-KEY': 'de275520-e304-476e-9a0d-a973f6702948',
+            'X-API-KEY': 'abd02b1b-dde9-46ba-8064-05bb7110fda3',
             "Content-Type": "application/json"
         }
     })
