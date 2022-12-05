11c7312e6b0fd7647b953355288de929c636bfd0
Zh.Zhukovets <zhuk.zhenya2704@yandex.by>
2022-12-06 00:41:45 +0300

    add

--- src/store/AsynsStore/posts.tsx
+++ src/store/AsynsStore/posts.tsx
@@ -2,7 +2,7 @@ export const getPosts = async (handler?: any, pageCount=1) => {
   const response = await fetch(`https://kinopoiskapiunofficial.tech/api/v2.2/films?page=${pageCount}`, {
       method: "GET",
       headers: {
-          'X-API-KEY': '7c432a31-0918-4a1a-be4b-b0ffd31c5888',
+          'X-API-KEY': '04a2ea85-90da-42e4-9240-0471c2be5d51',
           "Content-Type": "application/json"
       }
   })
@@ -15,7 +15,7 @@ export const getTrendsPosts = async (handler: any, pagesCount=1) => {
     const response = await fetch(`https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=${pagesCount}`, {
         method: "GET",
         headers: {
-            'X-API-KEY': '7c432a31-0918-4a1a-be4b-b0ffd31c5888',
+            'X-API-KEY': '04a2ea85-90da-42e4-9240-0471c2be5d51',
             "Content-Type": "application/json"
         }
     })
@@ -28,7 +28,7 @@ export const getSearchPosts = async (handler:any,keyword:string, pagesCount=1) =
     const response = await fetch(`https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${keyword}&page=${pagesCount}`, {
         method: "GET",
         headers: {
-            'X-API-KEY': '7c432a31-0918-4a1a-be4b-b0ffd31c5888',
+            'X-API-KEY': '04a2ea85-90da-42e4-9240-0471c2be5d51',
             "Content-Type": "application/json"
         }
     })
